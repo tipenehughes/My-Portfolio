a1d1ee0d329215f8c03db686706817d36216c384
Tipene Hughes <tipenehughes@gmail.com>
2020-09-23 13:11:59 -0400

    added scrollFuntion for internal links and adjusted padding on containers to fit within navbar

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Scroll bar
+// Nav bar grow/collapse on scroll
 const navBar = document.getElementById("nav");
 
 function scrollFunction() {
@@ -15,6 +15,36 @@ function scrollFunction() {
 window.onscroll = function () {
     scrollFunction();
 };
+
+// Navbar scrolling links
+
+const aboutNav = document.getElementById("aboutNav");
+const portfolioNav = document.getElementById("portfolioNav");
+const contactNav = document.getElementById("contactNav");
+
+aboutNav.addEventListener("click", function (e) {
+    e.preventDefault();
+    about.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+portfolioNav.addEventListener("click", function (e) {
+    e.preventDefault();
+    portfolio.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+contactNav.addEventListener("click", function (e) {
+    e.preventDefault();
+    contact.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+// Home CTA button scrollFunction
+
+const homeCTA = document.getElementById("homeCTA");
+
+homeCTA.addEventListener("click", (e) => {
+    e.preventDefault();
+    portfolio.scrollIntoView({ behavior: "smooth", block: "start" });
+});
 
 // Hamburger menu
 
@@ -34,6 +64,25 @@ for (let i = 0; i < menuOptions.length; i++) {
         menuOverlay.classList.remove("is-visible");
         if (body.classList.contains("noscroll")) {
             body.classList.remove("noscroll");
+        }
+    });
+}
+
+// Hamburger menu scrollFunction
+
+const about = document.getElementById("about");
+const portfolio = document.getElementById("portfolio");
+const contact = document.getElementById("contact");
+
+for (let i = 0; i < menuOptions.length; i++) {
+    menuOptions[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        if (menuOptions[i] == menuOptions[0]) {
+            about.scrollIntoView({ behavior: "smooth", block: "start" });
+        } else if (menuOptions[i] == menuOptions[1]) {
+            portfolio.scrollIntoView({ behavior: "smooth", block: "start" });
+        } else if (menuOptions[i] == menuOptions[2]) {
+            contact.scrollIntoView({ behavior: "smooth", block: "start" });
         }
     });
 }
