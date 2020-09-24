8374427badbb5a0ac31942b824559fc67d40ba88
Tipene Hughes <tipenehughes@gmail.com>
2020-09-24 13:37:02 -0400

    Added form validation and form handling through netlify

--- app.js
+++ app.js
@@ -86,3 +86,71 @@ for (let i = 0; i < menuOptions.length; i++) {
         }
     });
 }
+
+// Form Validation
+
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+const submitBtn = document.getElementById("submit");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs() {
+    // get values from inputs
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim();
+
+    if (nameValue === "") {
+        setErrorFor(name, "Name cannot be blank");
+    } else {
+        setSuccessFor(name);
+    }
+    if (emailValue === "") {
+        setErrorFor(email, "Email cannot be blank");
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, "Email is not valid");
+    } else {
+        setSuccessFor(email);
+    }
+    if (messageValue === "") {
+        setErrorFor(message, "Message cannot be blank");
+    } else {
+        setSuccessFor(message);
+    }
+
+    if (
+        nameValue !== "" &&
+        messageValue !== "" &&
+        emailValue !== "" &&
+        isEmail(emailValue)
+    ) {
+        form.submit();
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+
+    small.innerText = message;
+
+    formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
+}
